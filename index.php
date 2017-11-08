2da8cba23c6a34ff06df5725f6f5389ecad51557
cgddgc <cgddgc@foxmail.com>
2017-11-08 17:49:13 +0800

    ch9

--- index.php
+++ index.php
@@ -4,8 +4,7 @@
 include 'wechat.class.php';
 #define("TOKEN", "zzzero"); 
 $options = array('token'=>'zzzero');
-$a="mywechat";
-$we = new $a($options);
+$we = new mywechat($options);
 $wechat=new Wechat($options);
 $wechat->valid();//明文或兼容模式可以在接口验证通过后注释此句，但加密模式一定不能注释，否则会验证失败
 $type = $wechat->getRev()->getRevType();
