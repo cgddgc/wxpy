98174402325fdf021f0430e72952551928890a2a
cgddgc <cgddgc@foxmail.com>
2017-12-14 17:06:28 +0800

    1215

---
+++ server.py
@@ -0,0 +1,21 @@
+#!python3
+#!coding=utf-8 
+
+from http.server import HTTPServer,BaseHTTPRequestHandler     
+import io,shutil,urllib     
+
+class MyHttpHandler(BaseHTTPRequestHandler):     
+    def do_GET(self):       
+        if '?' in self.path:     
+            self.queryString=urllib.parse.unquote(self.path.split('?',1)[1])          
+            params=urllib.parse.parse_qs(self.queryString)     
+            #print(params)     
+        self.send_response(200)             
+    def do_POST(self):     
+        s=str(self.rfile.readline(),'utf-8')  
+        print(s) 
+        print(urllib.parse.parse_qs(urllib.parse.unquote(s)))
+        self.send_response(301)
+pyhttpd=HTTPServer(('',8998),MyHttpHandler)     
+print("Server started on 127.0.0.1,port 8998.....")     
+pyhttpd.serve_forever()
