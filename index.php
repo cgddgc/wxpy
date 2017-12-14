4be8ec30b5583016fe8d99b7b43260e7eb0aa6fa
cgddgc <cgddgc@foxmail.com>
2017-12-14 11:54:37 +0800

    1214

--- index.php
+++ index.php
@@ -414,10 +414,10 @@ $we = new mywechat($options);
 #$we->weObj->valid();//明文或兼容模式可以在接口验证通过后注释此句，但加密模式一定不能注释，否则会验证失败
 $type = $we->weObj->getRev()->getRevType();
 $we->responseMsg($type);
-$users=$we->getUserList();
+#$users=$we->getUserList();
 echo "odd2gjtRc-ohwDhgqi19sn5Z5kUE";
-$pushdata={"touser"=>array("odd2gjtRc-ohwDhgqi19sn5Z5kUE"),"msgtype"=>"text","text"=>array("content"=>"hello wolrd")};
-$we->sendMassMessage($pushdata);
+#$pushdata={"touser"=>array("odd2gjtRc-ohwDhgqi19sn5Z5kUE"),"msgtype"=>"text","text"=>array("content"=>"hello wolrd")};
+#$we->sendMassMessage($pushdata);
 /*
 $test =new mywechat($options);
 $a=$test->respon(".python");
