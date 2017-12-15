7930fffd1666758901e0f992c6691159e322f01c
cgddgc <cgddgc@foxmail.com>
2017-12-16 03:02:56 +0800

    1216

--- wechat.py
+++ wechat.py
@@ -1,16 +1,15 @@
 #coding=utf-8
-import werobot
+import werobot,urllib,requests
 
 token="cgddgc"
 
 robot=werobot.WeRoBot(token=token)
-'''
+
 def TulingRobot(message):
     url = "http://www.tuling123.com/openapi/api"#'http://www.xiaodoubi.com/bot/chat.php'
-    r = requests.post(url, data={'key':"b8bb8bf591af8b522652fc2aa1e4a03a",'info':"lalala",'userid':"cgddgc"})
+    r = requests.post(url, data={'key':"b8bb8bf591af8b522652fc2aa1e4a03a",'info':message.content,'userid':message.source})
     result=(json.loads((r.text))["text"])
     return result
-'''
 
 @robot.text
 def hl(message):
