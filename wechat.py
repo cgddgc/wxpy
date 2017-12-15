6e1238ab03f9a4ccf5f8ff108d37f92671d485b8
cgddgc <cgddgc@foxmail.com>
2017-12-16 02:43:44 +0800

    1216

--- wechat.py
+++ wechat.py
@@ -7,12 +7,14 @@ robot=werobot.WeRoBot(token=token)
 
 @robot.text
 def hl(message):
-    return Message.content+"蠢材"
+    return message.content+"蠢材"
 
 @robot.image
-def h2():
+def h2(message):
     return message.img
-
+@robot.event
+def echoevent(message):
+    return message.event
 robot.config['HOST']='0.0.0.0'
 robot.config['PORT']=8998
 robot.run()
