eb6d76921560a1f27ebc969642266cb238cf5e0d
cgddgc <cgddgc@foxmail.com>
2017-12-15 15:50:06 +0800

    1216

--- server.py
+++ server.py
@@ -3,8 +3,17 @@
 
 from http.server import HTTPServer,BaseHTTPRequestHandler     
 import io,shutil,urllib,json     
+from wechatpy.utils import check_signature
+from wechatpy.exceptions import InvalidSignatureException
+from wechatpy import parse_message
 
-class MyHttpHandler(BaseHTTPRequestHandler):     
+
+
+token='cgddgc'
+
+class MyHttpHandler(BaseHTTPRequestHandler):
+    def __init__():
+        self.token=token
     def do_GET(self):       
         if '?' in self.path:     
             self.queryString=urllib.parse.unquote(self.path.split('?',1)[1])          
@@ -14,7 +23,9 @@ class MyHttpHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         self.queryString=urllib.parse.unquote(self.path.split('?',1)[1])          
         pstr=urllib.parse.parse_qs(self.queryString)
-        print(pstr["signature"][0],pstr["nonce"][0],pstr["timestamp"][0])
+        #signature=pstr["signature"][0]
+        #nonce=pstr["nonce"][0]
+        #timestam=pstr["timestamp"][0]
         #s=str(self.rfile.readline().decode(),'utf-8')  
         s=self.rfile.readlines()
         #s=self.request.recv(2048).strip()
@@ -25,8 +36,29 @@ class MyHttpHandler(BaseHTTPRequestHandler):
         xml=''
         xml=xml.join(list(s))
         print(xml)
+        return pstr,xml
         #print(urllib.parse.parse_qs(urllib.parse.unquote(s)))
         self.send_response(301)
+
+    def valid(self):
+        pstr,xml=self.do_POST()
+        timestamp=pstr["timestamp"][0]
+        nonce=pstr["nonce"][0]
+        signature=pstr["signature"][0]
+        try:
+            check_signature(token, signature, timestamp, nonce)
+        except InvalidSignatureException:
+            print('error')
+            pass 
+    def responMsg(self):
+        pstr,xml=self.do_POST()
+        msg=parse_message(xml)
+        print(msg)
 pyhttpd=HTTPServer(('',8998),MyHttpHandler)     
 print("Server started on 127.0.0.1,port 8998.....")     
 pyhttpd.serve_forever()
+autores=MyHttpHandler()
+while 1:
+    autores.responMsg()
+    pass
+
