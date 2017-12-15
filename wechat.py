ad17cd9bc4bb48f8f5a32e8ef91a56f717ee4d74
cgddgc <cgddgc@foxmail.com>
2017-12-16 03:22:09 +0800

    1216

--- wechat.py
+++ wechat.py
@@ -13,7 +13,7 @@ def TulingRobot(message):
 
 @robot.text
 def responText(message):
-    if (message.type=='img'):
+    if (message.__type__=='image'):
         @robot.image
         def resp2(message):
             return message.img
