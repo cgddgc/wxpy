196b67c00ec155f5282f7b9426fe9f29e6ed71d7
cgddgc <cgddgc@foxmail.com>
2017-12-16 00:48:44 +0800

    1216

--- server.py
+++ server.py
@@ -58,7 +58,7 @@ class MyHttpHandler(BaseHTTPRequestHandler):
         reply = TextReply(message=msg)
         reply.content = '你好啊，扑街！'
         restr=reply.render()
-        returncode=str(bytes(restr,encoding='utf-8'),encoding'utf-8').replace('\n','')
+        returncode=str(bytes(restr,encoding='utf-8'),encoding='utf-8').replace('\n','')
         self.wfile.write(returncode)
         print(msg,returncode)
 
