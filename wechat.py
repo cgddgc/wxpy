9a8a323a128c70e2df3e011b70d204c59ff19035
cgddgc <cgddgc@foxmail.com>
2017-12-16 02:39:09 +0800

    1216

--- wechat.py
+++ wechat.py
@@ -4,11 +4,16 @@ import werobot
 token="cgddgc"
 
 robot=werobot.WeRoBot(token=token)
-robot.config.update(HOST='0.0.0.0',PORT=8998)
+message=robot.Message
 
 @robot.text
-def hl():
-    return "蠢材"
+def hl(message):
+    return Message.content+"蠢材"
 
+@robot.image
+def h2():
+    return message.img
 
+robot.config['HOST']='0.0.0.0'
+robot.config['PORT']=8998
 robot.run()
