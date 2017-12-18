f04fc8748ff821ec1f217860e2951379dd66961c
cgddgc <cgddgc@foxmail.com>
2017-12-18 11:56:57 +0800

    1217

---
+++ wxcfg.py
@@ -0,0 +1,9 @@
+class MyConfig(object):
+    TOKEN="cgddgc",
+    SERVER="waitress",
+    HOST="0.0.0.0",
+    PORT=8998,
+    SESSION_STORAGE=None,
+    APP_ID="wx03b529f87a6d25a7",
+    APP_SECRET="678d29e1b19f6e5a3beb110e9699b136",
+    ENCODING_AES_KEY=None
