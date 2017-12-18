e1eac4b7366ef43c2a947a332cae98b118181824
cgddgc <cgddgc@foxmail.com>
2017-12-18 11:22:46 +0800

    1217

--- wechat.py
+++ wechat.py
@@ -1,12 +1,13 @@
 #coding=utf-8
 import werobot,urllib,requests,json
 
-#token="cgddgc"
-#robot.config['HOST']='0.0.0.0'
-#robot.config['PORT']=8998
+
 
 robot=werobot.WeRoBot()
-robot.config.from_pyfile(".weconfig")
+robot.config['token']="cgddgc"
+robot.config['HOST']='0.0.0.0'
+robot.config['PORT']=8998
+#robot.config.from_pyfile(".weconfig")
 
 def TulingRobot(message):
     url = "http://www.tuling123.com/openapi/api"#'http://www.xiaodoubi.com/bot/chat.php'
